ca2ef323d64a2f9d0dec9bb04f372ae94b6a3324
hupwy <albertini_peter@163.com>
2020-10-04 11:00:01 +0800

    comment feign client

--- k8s-srv-user/src/main/java/com/neusoft/edu/feign/MonitorService.java
+++ k8s-srv-user/src/main/java/com/neusoft/edu/feign/MonitorService.java
@@ -4,8 +4,6 @@ import com.neusoft.edu.feign.monitor.MonitorInterface;
 import com.neusoft.edu.feign.user.UserInterface;
 import org.springframework.cloud.openfeign.FeignClient;
 
-//@FeignClient(name = "http://192.168.2.11:32025", url = "http://192.168.2.11:32025", fallback = UserFallback.class)
-//@FeignClient(name = "http://localhost:8082", url = "http://localhost:8082", fallback = UserFallback.class)
 @FeignClient(name = "k8s-srv-docker-monitor", url = "${url.local:}", fallback = DockerFallback.class)
 public interface MonitorService extends MonitorInterface {
 
