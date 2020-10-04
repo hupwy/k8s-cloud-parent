cfbfc4eb9797106c79614ab9f10fed209a78fa04
hupwy <albertini_peter@163.com>
2020-10-04 11:01:22 +0800

    comment feign client

--- k8s-srv-user/src/main/java/com/neusoft/edu/feign/MonitorService.java
+++ k8s-srv-user/src/main/java/com/neusoft/edu/feign/MonitorService.java
@@ -1,7 +1,6 @@
 package com.neusoft.edu.feign;
 
 import com.neusoft.edu.feign.monitor.MonitorInterface;
-import com.neusoft.edu.feign.user.UserInterface;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "k8s-srv-docker-monitor", url = "${url.local:}", fallback = DockerFallback.class)
