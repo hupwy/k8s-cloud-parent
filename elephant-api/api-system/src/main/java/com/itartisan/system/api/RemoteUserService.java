6b1707a3e30974352d240b1896f4327ee68b197f
hup <albertini_peter@163.com>
2020-10-13 17:12:16 +0800

    system-api add

---
+++ elephant-api/api-system/src/main/java/com/itartisan/system/api/RemoteUserService.java
@@ -0,0 +1,27 @@
+package com.itartisan.system.api;
+
+import com.itartisan.common.core.constant.ServiceNameConstants;
+import com.itartisan.common.core.domain.R;
+import com.itartisan.system.api.factory.RemoteUserFallbackFactory;
+import com.itartisan.system.api.model.LoginUser;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * 用户服务
+ * 
+ * @author ruoyi
+ */
+@FeignClient(contextId = "remoteUserService", value = ServiceNameConstants.SYSTEM_SERVICE, fallbackFactory = RemoteUserFallbackFactory.class)
+public interface RemoteUserService
+{
+    /**
+     * 通过用户名查询用户信息
+     *
+     * @param username 用户名
+     * @return 结果
+     */
+    @GetMapping(value = "/user/info/{username}")
+    public R<LoginUser> getUserInfo(@PathVariable("username") String username);
+}
