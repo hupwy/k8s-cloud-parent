53fa18d71a889dde4ffb0201e289a5b6957267da
hup <albertini_peter@163.com>
2020-10-13 16:32:49 +0800

    common-core add

---
+++ elephant-common/common-core/src/main/java/com/itartisan/cn/common/core/constant/ServiceNameConstants.java
@@ -0,0 +1,17 @@
+package com.itartisan.cn.common.core.constant;
+
+/**
+ * 服务名称
+ */
+public interface ServiceNameConstants
+{
+    /**
+     * 认证服务的serviceid
+     */
+    String AUTH_SERVICE = "auth-redis";
+
+    /**
+     * 系统模块的serviceid
+     */
+    String SYSTEM_SERVICE = "system";
+}
---
+++ elephant-common/common-core/src/main/java/com/itartisan/cn/common/core/exception/PreAuthorizeException.java
@@ -0,0 +1,11 @@
+package com.itartisan.cn.common.core.exception;
+
+/**
+ * 权限异常
+ */
+public class PreAuthorizeException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public PreAuthorizeException() {
+    }
+}
