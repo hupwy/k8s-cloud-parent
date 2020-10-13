68279c433f95ad27b198c0fcfd7fe1cf986f4131
hup <albertini_peter@163.com>
2020-10-13 17:17:11 +0800

    RemoteUserService.java update

--- elephant-api/api-system/src/main/java/com/itartisan/system/api/RemoteUserService.java
+++ elephant-api/api-system/src/main/java/com/itartisan/system/api/RemoteUserService.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * 用户服务
- * 
+ *
  * @author ruoyi
  */
 @FeignClient(contextId = "remoteUserService", value = ServiceNameConstants.SYSTEM_SERVICE, fallbackFactory = RemoteUserFallbackFactory.class)
-public interface RemoteUserService
-{
+public interface RemoteUserService {
     /**
      * 通过用户名查询用户信息
      *
@@ -23,5 +22,5 @@ public interface RemoteUserService
      * @return 结果
      */
     @GetMapping(value = "/user/info/{username}")
-    public R<LoginUser> getUserInfo(@PathVariable("username") String username);
+    R<LoginUser> getUserInfo(@PathVariable("username") String username);
 }
