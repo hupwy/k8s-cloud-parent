a7c5f847170c052703ad68af7f7ff19e2151cf84
hup <albertini_peter@163.com>
2021-02-22 14:58:02 +0800

    整理lab，content服务的api接口

---
+++ elephant-api/api-system/src/main/java/com/itartisan/api/system/RemoteSysMessageService.java
@@ -0,0 +1,23 @@
+package com.itartisan.api.system;
+
+import com.itartisan.api.system.beans.domain.SysMessage;
+import com.itartisan.common.core.domain.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * 站内信服务
+ */
+@FeignClient(contextId = "remoteSysMessageService", value = "system", url = "${local.feign.server.system.url:}")
+public interface RemoteSysMessageService {
+    /**
+     * 向某些用户发布消息
+     *
+     * @param userIds
+     * @param sysMessage
+     * @return
+     */
+    @PostMapping("/message/publish/user/{userIds}")
+    R<SysMessage> publishUser(@PathVariable("userIds") Long[] userIds, SysMessage sysMessage);
+}
